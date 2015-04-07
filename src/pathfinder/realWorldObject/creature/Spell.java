37efb13a7a4cb0fde4596e335df9f26ddbbb8894
MattClucas <clucas@iastate.edu>
2015-04-07 16:31:57 -0500

    The skeleton of the spell class

---
+++ src/pathfinder/realWorldObject/creature/Spell.java
@@ -0,0 +1,24 @@
+package pathfinder.realWorldObject.creature;
+import java.util.List;
+
+import pathfinder.realWorldObject.Item;
+
+public class Spell {
+	
+	private String name;
+	private SpellSchool school;
+	//TODO TIME
+	//TODO ENERGY TYPE
+	//TODO energyTypes
+	//TODO levelReqs
+	private boolean verbal;
+	private boolean semantic;
+	List<Item> materials;
+	List<Item> focus;
+	List<Item> divineFocus;
+	int numTargets;
+	int baseRange;
+	int randModifier;
+	//TODO magical effects, and others to be determined
+
+}
