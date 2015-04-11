a8a83493ce876322c3930a6d505663b91dd3297d
Jacob Moyer <jamoyer@iastate.edu>
2015-04-11 18:02:42 -0500

    Fixed casting error in commoner class

--- src/pathfinder/characters/classes/npcClasses/Commoner.java
+++ src/pathfinder/characters/classes/npcClasses/Commoner.java
@@ -2,6 +2,7 @@ package pathfinder.characters.classes.npcClasses;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 import pathfinder.characters.baseAttackBonus.BaseAttackBonusProgression;
@@ -21,66 +22,63 @@ import pathfinder.realWorldObject.item.equipment.Proficiency;
 
 public class Commoner extends CharacterClass
 {
-	/*
-	 * Basic Class Stats
-	 */
-	private static final int HIT_DIE = 6;
-	private static final int SKILL_RANKS_PER_LEVEL = 2;
-	private static final BaseAttackBonusProgression BASE_ATTACK_BONUS = new SlowBAB();
+    /*
+     * Basic Class Stats
+     */
+    private static final int HIT_DIE = 6;
+    private static final int SKILL_RANKS_PER_LEVEL = 2;
+    private static final BaseAttackBonusProgression BASE_ATTACK_BONUS = new SlowBAB();
 
-	private static final BaseSavingThrow FORTITUDE = new BadSave();
-	private static final BaseSavingThrow REFLEX = new BadSave();
-	private static final BaseSavingThrow WILL = new BadSave();
-	private static final BaseSavingThrowSet BASE_SAVES = new BaseSavingThrowSet(
-			FORTITUDE, REFLEX, WILL);
+    private static final BaseSavingThrow FORTITUDE = new BadSave();
+    private static final BaseSavingThrow REFLEX = new BadSave();
+    private static final BaseSavingThrow WILL = new BadSave();
+    private static final BaseSavingThrowSet BASE_SAVES = new BaseSavingThrowSet(FORTITUDE, REFLEX, WILL);
 
-	// needs to be unmodifiable so that when this list is passed it is not
-	// accidentally modified, which could be disastrous
-	private static final List<String> SKILLS = Collections
-			.unmodifiableList(Arrays.asList(Climb.NAME, Craft.NAME,
-					HandleAnimal.NAME, Perception.NAME, Profession.NAME,
-					Ride.NAME, Swim.NAME));
+    // needs to be unmodifiable so that when this list is passed it is not
+    // accidentally modified, which could be disastrous
+    private static final List<String> SKILLS = Collections.unmodifiableList(Arrays.asList(Climb.NAME, Craft.NAME,
+            HandleAnimal.NAME, Perception.NAME, Profession.NAME, Ride.NAME, Swim.NAME));
 
-	private static final List<Proficiency> PROFICIENCIES = Collections.unmodifiableList(Arrays.asList());
+    private static final List<Proficiency> PROFICIENCIES = Collections.unmodifiableList(new LinkedList<Proficiency>());
 
-	@Override
-	public int getHitDieType()
-	{
-		return HIT_DIE;
-	}
+    @Override
+    public int getHitDieType()
+    {
+        return HIT_DIE;
+    }
 
-	@Override
-	public BaseAttackBonusProgression getBaseAttackBonusProgression()
-	{
-		return BASE_ATTACK_BONUS;
-	}
+    @Override
+    public BaseAttackBonusProgression getBaseAttackBonusProgression()
+    {
+        return BASE_ATTACK_BONUS;
+    }
 
-	@Override
-	public BaseSavingThrowSet getSavingThrowSet()
-	{
-		return BASE_SAVES;
-	}
+    @Override
+    public BaseSavingThrowSet getSavingThrowSet()
+    {
+        return BASE_SAVES;
+    }
 
-	@Override
-	public int getSkillRanksPerLevel()
-	{
-		return SKILL_RANKS_PER_LEVEL;
-	}
+    @Override
+    public int getSkillRanksPerLevel()
+    {
+        return SKILL_RANKS_PER_LEVEL;
+    }
 
-	@Override
-	public List<String> getClassSkills()
-	{
-		return SKILLS;
-	}
+    @Override
+    public List<String> getClassSkills()
+    {
+        return SKILLS;
+    }
 
-	@Override
-	public List<Proficiency> getProficiencies()
-	{
-		return PROFICIENCIES;
-	}
+    @Override
+    public List<Proficiency> getProficiencies()
+    {
+        return PROFICIENCIES;
+    }
 
-	/*
-	 * TODO class features
-	 */
+    /*
+     * TODO class features
+     */
 
 }
